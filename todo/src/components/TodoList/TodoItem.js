67347b3417c7034081e55b32a00c22e69b536674
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-18 13:04:54 +0100

    Add action dispatchers to TodoItem component

--- todo/src/components/TodoList/TodoItem.js
+++ todo/src/components/TodoList/TodoItem.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { toggleTodo, deleteTodo } from "../../actions/index";
 
-const TodoItem = () => {
-    return (
-        <div>TodoItem</div>
-    )
-}
+const TodoItem = props => {
+  const { value, completed } = props;
+  const { toggleTodo, deleteTodo } = props;
 
-export default TodoItem;
+  return (
+    <div>
+      <p>{value}</p>
+      <button>{`${completed}`}</button>
+    </div>
+  );
+};
+
+export default connect(
+  null,
+  { toggleTodo, deleteTodo }
+)(TodoItem);
--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
 import TodoItem from "./TodoItem";
-import { toggleTodo, deleteTodo } from "../../actions/index";
 
 const TodoList = props => {
   const { todos } = props;
