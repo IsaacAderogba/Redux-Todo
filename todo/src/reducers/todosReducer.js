e1bc8eb8f1635a1792b523b7b7b673c5b7e953f5
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-18 12:38:54 +0100

    Build Todo reducer

---
+++ todo/src/reducers/todosReducer.js
@@ -0,0 +1,20 @@
+import dummyTodos from "../constants/dummyTodos";
+import { ADD_TODO, TOGGLE_TODO, DELETE_TODO } from "../actions/index";
+
+export default (state = dummyTodos, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, action.payload];
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== action.payload);
+    case TOGGLE_TODO:
+      return state.map(todo => {
+          if(todo.id === action.payload) {
+              return { ...todo, completed: !todo.completed}
+          }
+          return todo;
+      })
+    default:
+      return state;
+  }
+};
