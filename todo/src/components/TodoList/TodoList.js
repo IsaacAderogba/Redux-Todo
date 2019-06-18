8b7d06b245a4d82d9c5279e78edc626eb18bc2d6
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-06-18 12:56:27 +0100

    Create mapStateToProps function

--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import TodoItem from './TodoItem';
-import { addTodo, toggleTodo, deleteTodo } from '../../actions/index';
+import { toggleTodo, deleteTodo } from '../../actions/index';
 
 const TodoList = () => {
     return (
         <div><TodoItem /></div>
     )
+}
+
+function mapStateToProps(state) {
+    return {
+        todos: state.todos,
+    }
 }
 
 export default TodoList;
